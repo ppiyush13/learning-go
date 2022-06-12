f1cae501f272d40e6fc755df0710c260f6a600f7
Piyush Lodaya <ppiyush13@gmail.com>
2022-06-12 17:17:01 +0530

    additional tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 /*
 	1. Length of new deck
@@ -13,5 +16,34 @@ func TestNewDeck(t *testing.T) {
 
 	if len(d) != 20 {
 		t.Errorf("Expected deck of length of 20, but got %v", len(d))
+	}
+}
+
+func TestFirstCard(t *testing.T) {
+	d := newDeck()
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+}
+
+func TestLastCard(t *testing.T) {
+	d := newDeck()
+
+	if d[len(d)-1] != "Five of Hearts" {
+		t.Errorf("Expected last card of Five of Hearts, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveTofile(t *testing.T) {
+	os.Remove("./tmp/_deck.dat")
+
+	deck := newDeck()
+	deck.saveToFile("./tmp/_deck.dat")
+
+	deck2 := newDeckFromFile("./tmp/_deck.dat")
+
+	if len(deck2) != 20 {
+		t.Errorf("Expected deck of length of 20, but got %v", len(deck2))
 	}
 }
